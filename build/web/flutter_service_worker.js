c4b6a1353743a788468d97c168f352616e5263a0
Camplesg <camplesg@csu.fullerton.edu>
2025-04-26 15:23:57 -0700

    prep

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bf7f16bba4864a8a63d4a5d4899f9e86
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "0537726b0bac9061f0957e6c47f701b6",
+"flutter_bootstrap.js": "eeb179de0e9707d95f29d888e4340c5d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
